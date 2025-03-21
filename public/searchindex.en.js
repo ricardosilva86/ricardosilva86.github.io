ef3144ffe2c73c11d4080ad8d8976c2a7d6ce2f5
Ricardo Silva <ricardolopes18@yahoo.com.br>
2025-03-21 09:18:26 +0100

    Added built website

---
+++ public/searchindex.en.js
@@ -0,0 +1,34 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "My Personal Blog",
+    "uri": "/index.html"
+  },
+  {
+    "breadcrumb": "My Personal Blog",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Posts",
+    "uri": "/posts/index.html"
+  },
+  {
+    "breadcrumb": "My Personal Blog",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "My Personal Blog",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
